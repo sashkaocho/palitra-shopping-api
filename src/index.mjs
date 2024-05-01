62397b4ef8999c8504f7c46598ed7125bb0bd96c
sashkaochigava <sashkaochi@gmail.com>
2024-05-02 02:43:05 +0400

    chore: used cart and product routers

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,29 @@
-import express from 'express'
+import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import productRouter from "./routes/product.routes.js";
+import cartRouter from "./routes/cart.routes.js";
 
-const app = express()
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-mongoose.connect("mongodb+srv://admin:Password1!@palitra-shop.lpkqpmf.mongodb.net/?retryWrites=true&w=majority&appName=palitra-shop")
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log('server is running')
-        })
-        console.log('connected')
-    }).catch(() => {
-    console.log('err')
-})
+app.use(express.json());
+app.use(cors());
+
+app.use(productRouter);
+app.use(cartRouter);
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:Password1!@palitra-shop.lpkqpmf.mongodb.net/?retryWrites=true&w=majority&appName=palitra-shop",
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("server is running");
+    });
+    console.log("connected to a database");
+  })
+  .catch(() => {
+    console.log("err");
+  });
