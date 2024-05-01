a9e650bec332d7979e1f5d274f0909b325d5dc11
sashkaochigava <sashkaochi@gmail.com>
2024-05-01 15:11:05 +0400

    feat: connected to a database

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,16 @@
 import express from 'express'
+import mongoose from "mongoose";
 
 const app = express()
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log('idiot')
+mongoose.connect("mongodb+srv://admin:Password1!@palitra-shop.lpkqpmf.mongodb.net/?retryWrites=true&w=majority&appName=palitra-shop")
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('server is running')
+        })
+        console.log('connected')
+    }).catch(() => {
+    console.log('err')
 })
